d9145554610877871374502c2fb91ac639ff8ef9
Cleverson Teotonio <cleverson_at@hotmail.com>
2019-05-01 16:14:55 -0300

    Convertion of temperature to music category done

--- backend-challenge/src/main/java/com/widesoftware/backendchallenge/domain/TemperatureToSongCategory.java
+++ backend-challenge/src/main/java/com/widesoftware/backendchallenge/domain/TemperatureToSongCategory.java
@@ -5,7 +5,21 @@ import com.widesoftware.backendchallenge.entities.SongsCategory;
 public class TemperatureToSongCategory {
 	
 	public SongsCategory convert(int temperature) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		if (temperature < 15) {
+			return SongsCategory.ROCK;
+		} else if (isInBetween(temperature, 15, 20)) {
+			return SongsCategory.CLASSIC;
+		} else if (isInBetween(temperature, 21, 30)) {
+			return SongsCategory.HIP_HOP;
+		} else if (temperature > 30) {
+			return SongsCategory.PARTY;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	private boolean isInBetween(int value, int lowerBound, int upperBound) {
+		return ((value >= lowerBound) && (value <= upperBound)) ? true : false;
 	}
 	
 }
